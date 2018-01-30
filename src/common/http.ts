38ac81a4ff7111e6b70ec26e5676b8ab23fcb17a
Shady Khalifa <shekohex@gmail.com>
2018-01-30 22:30:34 +0200

    add headers and timeout

--- src/common/http.ts
+++ src/common/http.ts
@@ -1,5 +1,10 @@
 import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 let Config = require('../config.json');
 export const HTTP = axios.create({
-  baseURL: `https://api.github.com/repos/${Config.repo}/contents/`
+  baseURL: `https://api.github.com/repos/${Config.repo}/contents/`,
+  timeout: 10000,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 });
